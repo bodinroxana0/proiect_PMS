a38af3562c1e1a229a45e3a7863d24486bf02712
Bodin Roxana <bodinroxana@yahoo.com>
2020-11-07 19:11:02 +0200

    Grafice sunet + FFT

--- Shazam/Shazam/Form1.Designer.cs
+++ Shazam/Shazam/Form1.Designer.cs
@@ -30,45 +30,94 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.scottPlotUC2 = new ScottPlot.ScottPlotUC();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(671, 301);
+            this.button3.Location = new System.Drawing.Point(251, 893);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(206, 82);
+            this.button3.Size = new System.Drawing.Size(206, 45);
             this.button3.TabIndex = 2;
             this.button3.Text = "Oprire";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(518, 236);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "00 : 00 00";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 301);
+            this.button1.Location = new System.Drawing.Point(24, 893);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(206, 82);
+            this.button1.Size = new System.Drawing.Size(206, 45);
             this.button1.TabIndex = 4;
             this.button1.Text = "Înregistrare";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scottPlotUC1.Location = new System.Drawing.Point(0, 0);
+            this.scottPlotUC1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Size = new System.Drawing.Size(1238, 396);
+            this.scottPlotUC1.TabIndex = 5;
+            // 
+            // scottPlotUC2
+            // 
+            this.scottPlotUC2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scottPlotUC2.Location = new System.Drawing.Point(0, 396);
+            this.scottPlotUC2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.scottPlotUC2.Name = "scottPlotUC2";
+            this.scottPlotUC2.Size = new System.Drawing.Size(1238, 491);
+            this.scottPlotUC2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(921, 893);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1038, 893);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "00:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1132, 893);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ms";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 608);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1238, 944);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.scottPlotUC2);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,9 +129,13 @@
 
         #endregion
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private ScottPlot.ScottPlotUC scottPlotUC2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Shazam/Shazam/Form1.cs
+++ Shazam/Shazam/Form1.cs
@@ -10,46 +10,71 @@ using NAudio;
 using System.Windows.Forms;
 using System.IO;
 using NAudio.Wave;
+using Accord.Math;
+using System.Numerics;
 
 namespace Shazam
 {
     public partial class Form1 : Form
     {
+        public static int miliseconds = 0;
+        public BufferedWaveProvider bwp;
+        public Int32 envelopeMax;
+
+        private int RATE = 44100; // sample rate of the sound card
+        private int BUFFERSIZE = (int)Math.Pow(2, 11); // must be a multiple of 2
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        
             var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
             Directory.CreateDirectory(outputFolder);
             var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
-            var waveIn = new WaveInEvent();
+            
+            // get the WaveIn class started
+            WaveIn waveIn = new WaveIn();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+            waveIn.BufferMilliseconds = (int)((double)BUFFERSIZE / (double)RATE * 1000.0);
+            
+            //prepare to store to buffer to plot the audio signal
+            bwp = new BufferedWaveProvider(waveIn.WaveFormat);
+            bwp.BufferLength = BUFFERSIZE * 2;
+            bwp.DiscardOnBufferOverflow = true;
+
             WaveFileWriter writer = null;
             bool closing = false;
             //pentru inregistrare audio
             button1.Click += (s, a) =>
             {
+                UpdateAudioGraph();
+                //start timer
+                timer1.Enabled = true;
+                //prepare to write to wav file
                 writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+                
+                //start recording
                 waveIn.StartRecording();
+                
                 button1.Enabled = false;
                 button3.Enabled = true;
             };
             waveIn.DataAvailable += (s, a) =>
             {
                 writer.Write(a.Buffer, 0, a.BytesRecorded);
-            };
-            waveIn.DataAvailable += (s, a) =>
-            {
-                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                bwp.AddSamples(a.Buffer, 0, a.BytesRecorded);
                 if (writer.Position > waveIn.WaveFormat.AverageBytesPerSecond * 30)
                 {
                     waveIn.StopRecording();
                 }
             };
             //pentru oprire inregistrare
-            button3.Click += (s, a) => waveIn.StopRecording();
+            button3.Click += (s, a) =>
+            {
+                //stop timer
+                timer1.Enabled = false;
+                waveIn.StopRecording();
+            };
             this.FormClosing += (s, a) => { closing = true; waveIn.StopRecording(); };
 
             waveIn.RecordingStopped += (s, a) =>
@@ -63,6 +88,83 @@ namespace Shazam
                     waveIn.Dispose();
                 }
             };
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void UpdateAudioGraph()
+        {
+            // read the bytes from the stream
+            int frameSize = BUFFERSIZE;
+            var frames = new byte[frameSize];
+            bwp.Read(frames, 0, frameSize);
+            if (frames.Length == 0) return;
+            if (frames[frameSize - 2] == 0) return;
+            
+
+            // convert it to int32 manually (and a double for scottplot)
+            int SAMPLE_RESOLUTION = 16;
+            int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+            Int32[] vals = new Int32[frames.Length / BYTES_PER_POINT];
+            double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs = new double[frames.Length / BYTES_PER_POINT];
+            double[] Ys2 = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs2 = new double[frames.Length / BYTES_PER_POINT];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                // bit shift the byte buffer into the right variable format
+                byte hByte = frames[i * 2 + 1];
+                byte lByte = frames[i * 2 + 0];
+                vals[i] = (int)(short)((hByte << 8) | lByte);
+                Xs[i] = i;
+                Ys[i] = vals[i];
+                Xs2[i] = (double)i / Ys.Length * RATE / 1000.0; // units are in kHz
+            }
+            
+            // update scottplot (PCM, time domain)
+            scottPlotUC1.Xs = Xs;
+            scottPlotUC1.Ys = Ys;
+
+            //update scottplot (FFT, frequency domain)
+            Ys2 = FFT(Ys);
+            scottPlotUC2.Xs = Xs2.Take(Xs2.Length / 2).ToArray();
+            scottPlotUC2.Ys = Ys2.Take(Ys2.Length / 2).ToArray();
+
+
+            // update the displays
+            scottPlotUC1.UpdateGraph();
+            scottPlotUC2.UpdateGraph();
+
+            Application.DoEvents();
+            scottPlotUC1.Update();
+            scottPlotUC2.Update();
+            
+
+        }
+        public double[] FFT(double[] data)
+        {
+            double[] fft = new double[data.Length]; // this is where we will store the output (fft)
+            Complex[] fftComplex = new Complex[data.Length]; // the FFT function requires complex format
+            for (int i = 0; i < data.Length; i++)
+            {
+                fftComplex[i] = new Complex(data[i], 0.0); // make it complex format (imaginary = 0)
+            }
+            Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+            for (int i = 0; i < data.Length; i++)
+            {
+                fft[i] = fftComplex[i].Magnitude; // back to double
+                //fft[i] = Math.Log10(fft[i]); // convert to dB
+            }
+            return fft;
+            //todo: this could be much faster by reusing variables
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            miliseconds++;
+            label2.Text = (timer1.Interval*miliseconds).ToString();
+            UpdateAudioGraph();
         }
     }
 }
